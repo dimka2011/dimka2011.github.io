ee05b3ea195965f4a35815106e8cbc567837f053
dimka2011 <Roditel.dfv2011@gmail.com>
2024-01-13 00:32:00 +0300

    up sent

--- send.php
+++ send.php
@@ -11,11 +11,12 @@ $text = urldecode($text);
 $name = trim($name);
 $email = trim($email);
 $text = trim($text);
-//echo $fio;
+//echo $name;
 //echo "<br>";
 //echo $email;
 if (mail("roditel.dfv@mail.ru", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email ,"Анекдот:".$text"From: info@dimka2011.github.io \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
+
 }?>
