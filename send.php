62097b309d7d6dc268182abb4013716df557fa7a
dimka2011 <Roditel.dfv2011@gmail.com>
2024-01-13 00:20:02 +0300

    зрз 1

---
+++ send.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$text = htmlspecialchars($text);
+$name = urldecode($name);
+$email = urldecode($email);
+$text = urldecode($text);
+$name = trim($name);
+$email = trim($email);
+$text = trim($text);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("roditel.dfv@mail.ru", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email ,"Анекдот:".$text"From: info@dimka2011.github.io \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
